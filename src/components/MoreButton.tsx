32a958f72589455da778e4c78b11fd609320c3ab
Marco <goettgens.marco@gmail.com>
2026-04-02 03:26:50 +0200

    chore: add download icon for file export

--- src/components/MoreButton.tsx
+++ src/components/MoreButton.tsx
@@ -1,4 +1,6 @@
+import DownloadIcon from "@mui/icons-material/Download";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import MenuItem from "@mui/material/MenuItem";
 import { useBookmarkStore } from "../hooks/useBookmarkStore";
@@ -15,6 +17,9 @@ export const MoreButton = () => {
           downloadBookmarksFile("bookmark_export.html", rootFolder.bookmarks);
         }}
       >
+        <ListItemIcon>
+          <DownloadIcon />
+        </ListItemIcon>
         <ListItemText primary={"Export Bookmarks"} sx={{ m: 0 }} />
       </MenuItem>
     </MenuButton>
